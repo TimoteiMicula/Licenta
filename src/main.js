3b56ae1258016c7e867b13b7e08870f1f7cfdb64
Timotei Micula <timotei.micula@emaneul.ro>
2020-12-21 20:22:55 +0200

    Login

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase/app";
+import "firebase/auth";
+import axios from "axios";
+
+
+
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAygaSZ3mDp8IxLDt9dKXW3H0xpJdxRjvs",
+  authDomain: "crud-78ffe.firebaseapp.com",
+  databaseURL: "https://crud-78ffe.firebaseio.com",
+  projectId: "crud-78ffe",
+  storageBucket: "crud-78ffe.appspot.com",
+  messagingSenderId: "834322012186",
+  appId: "1:834322012186:web:13ce05bf419a2fbad159a3",
+  measurementId: "G-C3CFRCYD75"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let app;
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log("user", user);
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
+
+
